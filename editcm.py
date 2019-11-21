d7d684910a9255733c8409d3bd9e7acc70fcb5f4
Jeandre Le Roux <jeandre.le.roux@lsd.co.za>
2019-11-21 12:48:26 +0200

    Initial commit

---
+++ editcm.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import subprocess, yaml, sys, tempfile
+
+cm_name = sys.argv[1]
+
+cm_in_yaml = subprocess.check_output(['oc', 'get', 'cm', '-o', 'yaml', cm_name])
+
+cm = yaml.load(cm_in_yaml)
+
+tmpdir = tempfile.mkdtemp()
+
+for file in cm['data']:
+    disk_file = f"{tmpdir}/{file}"
+    outfile = open(disk_file, 'w')
+    outfile.write(cm['data'][file])
+    outfile.close()
+    subprocess.run(['sensible-editor', disk_file])
+    cm['data'][file] = open(disk_file).read()
+
+cm_outfile = open(f"{tmpdir}/cm.yaml", 'w')
+yaml.dump(cm, cm_outfile)
+cm_outfile.close()
+
+subprocess.run(['oc', 'replace', '-f', f"{tmpdir}/cm.yaml"])
